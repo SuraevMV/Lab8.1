d9ebe29f3833d50cc97f0e85a7071a92f022e97e
Михаил <stulstoldevyat@gmail.com>
2022-02-21 00:46:35 +0300

    Добавьте файлы проекта.

---
+++ Lab8.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Lab8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random random = new Random();
+            string path = "Logs/Log.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                foreach (int a in numbers)
+                {
+                    numbers[a] = random.Next(0, 10);
+                    Console.Write(numbers[a]);
+                    sw.Write(numbers[a]);
+                }
+            }
+        }
+    }
+}
---
+++ Lab8.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Lab8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Папка/Файл.txt";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int numbWord = 0;
+                int numbSymbol = 0;
+                int numbLine = 0;
+                foreach (string line in File.ReadLines(path))
+                {
+                    numbLine++;
+                    string[] line1 = line.Split();
+                    foreach (string a in line1)
+                    {
+                        numbWord++;
+                        numbSymbol += a.Length;
+                    }
+                }
+
+                Console.WriteLine("Число строк: {0}\nЧисло слов: {1}\nЧисло символов: {2}", numbLine, numbWord, numbSymbol);
+                Console.ReadKey();
+            }
+        }
+    }
+}
